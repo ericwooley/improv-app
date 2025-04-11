44c20635aa3287869d8006d03f465bdba153c9fe
Eric Wooley <ericwooley@gmail.com>
2025-04-11 11:46:35 -0600

    set cors headers

--- main.go
+++ main.go
@@ -60,7 +60,7 @@ func main() {
 	// CORS middleware
 	r.Use(func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Origin", os.Getenv("FRONTEND_URL"))
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
